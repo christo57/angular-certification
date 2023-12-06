0a47d5859b12ed3f7b448e97be9601d53a23f5fc
christo57 <christopher.klein57@gmail.com>
2023-12-06 17:28:24 +0100

    feat(init): add prettier, eslint and material

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component} from "@angular/core";
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  standalone: true
+})
+export class HomeComponent {
+
+}
