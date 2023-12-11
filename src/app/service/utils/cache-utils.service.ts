d6a754e60da1ce7f8b622a187bf69d6b6e6c55d8
Christopher Klein <christopher.klein57@gmail.com>
2023-12-11 13:19:57 +0100

    fix cache

--- src/app/service/utils/cache-utils.service.ts
+++ src/app/service/utils/cache-utils.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Injectable, inject } from '@angular/core';
 import { Observable, of, tap } from 'rxjs';
 
 @Injectable()
 export class CacheUtilsService {
+  private readonly document = inject(DOCUMENT);
+
   public getCacheOrResult<T>(
     key: string,
     result: Observable<T>
   ): Observable<T> {
-    const cacheLeagueResult = localStorage.getItem(key);
+    const localStorage = this.document.defaultView?.localStorage;
+
+    const cacheLeagueResult = localStorage?.getItem(key);
     if (cacheLeagueResult) {
       console.log(`get res ${key} from cache`);
       return of(JSON.parse(cacheLeagueResult) as T);
     } else {
       console.log('get res from api service');
       return result.pipe(
-        tap(result => localStorage.setItem(key, JSON.stringify(result)))
+        tap(result => localStorage?.setItem(key, JSON.stringify(result)))
       );
     }
   }
